f52def77553204267c864070fd1e5486382bd126
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2019-10-27 15:51:44 -0300

    first commit

---
+++ features/steps_definitions/login_steps.rb
@@ -0,0 +1,33 @@
+Quando("realizar a requisição para logar na API") do
+  $payload = HTTParty.post(
+    "https://api-de-tarefas.herokuapp.com/sessions",
+    {
+      :headers => {"Content-Type" => "application/json",
+                  "Accept" => "application/vnd.taskmanager.v2"},
+      :body => JSON.generate({
+        "session": {
+          "email": CONFIG["email"],
+          "password": CONFIG["password"]
+        }
+      })
+    } 
+
+  )
+end
+
+Então("o sistema retorna o código {int}") do |int|
+  expect($payload.code).to eq(int)
+end
+
+Então("o token da url é gerado criando a sessão") do
+  puts 'Token: ' + $payload['data']['attributes']['auth-token']
+end
+
+Quando("realizar a requisição para fazer logou na API") do
+  $token = $payload['data']['attributes']['auth-token']
+  $payload = HTTParty.delete(
+    "https://api-de-tarefas.herokuapp.com/sessions/#{$token}",
+    :headers => {"Content-Type" => "application/json",
+      "Accept" => "application/vnd.taskmanager.v2"}
+  )
+end
